7a45fb2116429833f7fd336381e7b0018ab5ff97
Krzysztof Przygoda <32018059+KrzysztofPrzygoda@users.noreply.github.com>
2021-04-27 10:11:08 +0200

    Updated

--- linux/ssl-generate.sh
+++ linux/ssl-generate.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #######################################
 # Generate keys and SSL certificate for CA and site:
-#   CA key
-#   CA pem
-#   CA srl
-#   Site key
-#   Site csr
-#   Site ext
-#   Site crt
-#   Site pem
+#   CA key    CA RSA key
+#   CA pem    CA Certificate in PEM format
+#   CA srl    ?
+#   Site key  RSA key
+#   Site csr  Certificate Signing Request
+#   Site ext  Certificate information
+#   Site crt  Certificate
+#   Site pem  Certificate in PEM format
 # Reference:
-#   None
+#   Intro to Digital Certificates https://www.youtube.com/watch?v=qXLD2UHq2vk
 # Usage: sudo bash ssl-generate.sh [no-options]
 #   no-options    [Optional] There are no options provided currently.
 # Removes:
@@ -35,26 +35,34 @@ DNS.1 = my.organization.com
 DNS.2 = my2.organization.com
 "
 
+# country="PL"
+# state="dolnoslaskie"
+# city="Wroclaw"
+# org_name="Company Name"
+# org_unit="Company Section"
+# common_name="my.organization.com" # FQDN or YOUR name
+# email="my.email@organization.com"
 
 ## 1. Generate CA cert and key
 openssl genrsa -out ${CA_name}.key 4096
-# Generate key.
+# Generate CA key.
 openssl req -x509 -new -nodes -key ${CA_name}.key -sha256 -days 1825 -out ${CA_name}.pem
-# Generate certificate.
+# Generate CA certificate. Asks for organization info.
 
 # openssl x509 -in ${CA_name}.pem -text -noout
-# View certificate.
+# View CA certificate.
 
 ## 2. Generate site key
 openssl genrsa -out ${site}.key 2048
 
 ## 3. Generate CSR for site cert
 openssl req -new -key ${site}.key -out ${site}.csr
+# Asks for organization info.
 
 ## 4. Prepare ext file with SSL cert extensions
 echo ${site_extention} > ${site}.ext
 
-## 5. Generate site certificate using CAcert, CA key, site CSR and ext file
+## 5. Generate site certificate using CA cert, CA key, site CSR and ext file
 openssl x509 -req -in ${site}.csr -CA ${CA_name}.pem -CAkey ${CA_name}.key -CAcreateserial -out ${site}.crt -days 825 -sha256 -extfile ${site}.ext
 
 ## 6. Convert to PEM in case when app requires PEM format

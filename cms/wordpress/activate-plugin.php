01356a29e7490fd5217009615989fdbc3c170e00
Krzysztof Przygoda <przygoda.krzysztof@gmail.com>
2024-12-28 02:57:52 +0100

    Add file doc.

--- cms/wordpress/activate-plugin.php
+++ cms/wordpress/activate-plugin.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * Script for activating a plugin via PHP.
+ * Useful when a deactivated plugin
+ * cannot be reactivated from the admin panel.
+ */
+
 define('WP_USE_THEMES', false);
 require_once('./wp-load.php');
 require_once(ABSPATH . 'wp-admin/includes/plugin.php');

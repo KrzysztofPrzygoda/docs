e585d39e578f761c90d332610883bf9d0edcd45d
Krzysztof Przygoda <przygoda.krzysztof@gmail.com>
2026-02-18 11:32:55 +0100

    Comment out unused THREE.js import statement for clarity

--- dev/js/snippets/particles-3/js/mouse-tracker.js
+++ dev/js/snippets/particles-3/js/mouse-tracker.js
@@ -1,4 +1,5 @@
-import * as THREE from "https://unpkg.com/three@0.180.0/build/three.module.js";
+// import * as THREE from "https://unpkg.com/three@0.180.0/build/three.module.js";
+import * as THREE from 'three';
 const { Vector2 } = THREE;
 
 /**

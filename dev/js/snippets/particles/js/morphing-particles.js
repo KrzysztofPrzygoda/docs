cc9d1801da0550368256406d9b3702a5d8ce5c4a
Krzysztof Przygoda <przygoda.krzysztof@gmail.com>
2026-02-26 16:47:37 +0100

    Refactor color controls initialization in MorphingParticlesScene for improved clarity

--- dev/js/snippets/particles/js/morphing-particles.js
+++ dev/js/snippets/particles/js/morphing-particles.js
@@ -847,9 +847,9 @@ class MorphingParticlesScene {
     }
     async initScene() {
         this.colorControls = {
-            color1: this.theme === "dark" ? this.color1 || "#318bf7" : this.color1 || "#676A72",
-            color2: this.theme === "dark" ? this.color2 || "#bada4c" : this.color2 || "#FF4641",
-            color3: this.theme === "dark" ? this.color3 || "#e35058" : this.color3 || "#346BF1"
+            color1: this.color1,
+            color2: this.color2,
+            color3: this.color3
         };
         await this.initParticles();
         this.options.gui && this.initGUI();
